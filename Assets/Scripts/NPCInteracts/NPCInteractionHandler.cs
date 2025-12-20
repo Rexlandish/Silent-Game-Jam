35a8b68beba4cce94f2f52593fbd48d02fc213da
SageIndieDev <zerotriplethreee@gmail.com>
2025-12-20 23:01:04 +0000

    Camera Zoom Fix Yes

--- Assets/Scripts/NPCInteracts/NPCInteractionHandler.cs
+++ Assets/Scripts/NPCInteracts/NPCInteractionHandler.cs
@@ -1,82 +1,83 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using Unity.Cinemachine;
 
-public class NPCInteractionHandler : MonoBehaviour
-{
-    public NPCHandler Handle;
-    public float FovSpeed = 4f;
-    public float normalFOV = 40f;
-    public float targetFOV = 35f;
-    public bool playerexit = false;
-    public bool playerinside = false;
-
-    Camera cam;
-
-    private void Start()
+    public class NPCInteractionHandler : MonoBehaviour
     {
-        cam = Camera.main;
-    }
+        public NPCHandler Handle;
+        public float FovSpeed = 4f;
+        public float normalFOV = 40f;
+        public float targetFOV = 35f;
+        public bool playerexit = false;
+        public bool playerinside = false;
 
-    public void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        { 
-            playerinside = true;
-            StartCoroutine(FOVEffectEnter());
-            Handle.PlayerWithinRange();
-        }
-    }
+        public CinemachineCamera cam;
 
-    public void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
+        private void Start()
         {
-            playerexit = true;
-            Handle.PlayerUnreachable();
+            cam = GameObject.Find("PlayerCamera").GetComponent<CinemachineCamera>();
         }
-    }
 
-    void Update()
-    {
-        if (playerexit)
+        public void OnTriggerEnter(Collider other)
         {
-            StopCoroutine(FOVEffectEnter());
-            StartCoroutine(FOVEffectExit());
-        }
-    }
-    
-    
-    
-    private System.Collections.IEnumerator FOVEffectEnter()
-    {
-        if (playerinside)
-        {
-            // Step 1: Increase quickly to dashFOV
-            while (cam.fieldOfView > targetFOV + 0.1f)
-            {
-                cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, targetFOV, Time.deltaTime * FovSpeed);
-                yield return null;
+            if (other.CompareTag("Player"))
+            { 
+                playerinside = true;
+                StartCoroutine(FOVEffectEnter());
+                Handle.PlayerWithinRange();
             }
-
-            cam.fieldOfView = targetFOV; // snap cleanly
-            playerinside = false;
         }
-    }
-    
-    private System.Collections.IEnumerator FOVEffectExit()
-    {
-        if (playerexit)
+
+        public void OnTriggerExit(Collider other)
         {
-            while (cam.fieldOfView < normalFOV - 0.1f)
+            if (other.CompareTag("Player"))
             {
-                cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, normalFOV, Time.deltaTime * FovSpeed);
-                yield return null;
+                playerexit = true;
+                Handle.PlayerUnreachable();
             }
+        }
 
-            cam.fieldOfView = normalFOV; // snap cleanly
-            playerexit = false;
-            StopAllCoroutines();
+        void Update()
+        {
+            if (playerexit)
+            {
+                StopCoroutine(FOVEffectEnter());
+                StartCoroutine(FOVEffectExit());
+            }
+        }
+        
+        
+        
+        private System.Collections.IEnumerator FOVEffectEnter()
+        {
+            if (playerinside)
+            {
+                // Step 1: Increase quickly to dashFOV
+                while (cam.Lens.FieldOfView > targetFOV + 0.1f)
+                {
+                    cam.Lens.FieldOfView = Mathf.Lerp(cam.Lens.FieldOfView, targetFOV, Time.deltaTime * FovSpeed);
+                    yield return null;
+                }
+
+                cam.Lens.FieldOfView = targetFOV; // snap cleanly
+                playerinside = false;
+            }
+        }
+        
+        private System.Collections.IEnumerator FOVEffectExit()
+        {
+            if (playerexit)
+            {
+                while (cam.Lens.FieldOfView < normalFOV - 0.1f)
+                {
+                    cam.Lens.FieldOfView = Mathf.Lerp(cam.Lens.FieldOfView, normalFOV, Time.deltaTime * FovSpeed);
+                    yield return null;
+                }
+
+                cam.Lens.FieldOfView = normalFOV; // snap cleanly
+                playerexit = false;
+                StopAllCoroutines();
+            }
         }
     }
-}
