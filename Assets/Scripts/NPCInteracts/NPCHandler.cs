871ca06088ba32e460fd41ff98d7ccb82f722d3b
SageIndieDev <zerotriplethreee@gmail.com>
2025-12-21 02:26:05 +0000

    Skybox

--- Assets/Scripts/NPCInteracts/NPCHandler.cs
+++ Assets/Scripts/NPCInteracts/NPCHandler.cs
@@ -33,8 +33,9 @@
 
         void Update()
         {
-            if (canInteract == true && Translations.gameObject.activeInHierarchy == false)
+            if (canInteract == true)
             {
+                if ( Translations.gameObject.activeInHierarchy == false)
                     if (Input.GetKeyDown(KeyCode.E))
                     {
                         // DialogueBox.SetActive(true);
